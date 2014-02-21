a47d434bb52664ac6ebd7cb014c1e6ef5ad940e7
Ben Gundersen <ben@bengundersen.com>
2014-02-21 01:45:15 -0500

    Starting new repo for app

---
+++ app/App.js
@@ -0,0 +1,91 @@
+var App = function() {
+  this.init();
+}
+App.prototype.init = function(options) {
+  console.log("App init");
+  new NoClickDelay(document.getElementById('menu'));
+  new NoClickDelay(document.getElementById('header'));
+}
+
+App.View = function(options) {
+  var t = this;
+  var parts, ev, evname, sel;
+  $.extend(t, options);
+  t.$el = $('#'+options.id);
+  t.$template = $('#'+options.template);
+  console.log("Constructing view: "+options.id);
+  for(ev in t.events) {
+    if(t.events.hasOwnProperty(ev)) {
+      parts = ev.split(" ");
+      evname = parts.shift();
+      sel = parts.join(" ");
+      console.log("Setting "+evname+" event for #"+t.$el.attr('id')+" "+sel);
+      (function(evt) {
+        t.$el.on(evname, sel, function() {
+          var args = Array.prototype.slice.call(arguments);
+          t.events[evt].apply(t, args);
+        });
+      })(ev);
+    }
+  }
+  if(t.init) {
+    t.init.apply(t);
+  }
+}
+
+
+App.Model = function(options) {
+  var t = this;
+  t.url = options.url;
+  t.parse = options.parse;
+}
+App.Model.prototype.load = function(callback) {
+  var t = this;
+  $.ajax({
+    url: t.url,
+    type: 'GET',
+    dataType: 'json',
+    success: function(resp) {
+      if(typeof t.parse === 'function') {
+        t.data = t.parse.apply(t, [resp]);
+      } else {
+        t.data = resp;
+      }
+      callback.apply(t, [t.data]);
+    }
+  });
+}
+
+
+App.Router = function(options) {
+  var defaults = {
+    containerId: "pages",
+    pageClass: "page",
+    activeClass: "active"
+  }
+  this.options = $.extend({}, defaults, options);
+}
+App.Router.prototype.start = function() {
+  console.log("Router start");
+  var t = this;
+  t.context = t.options.context;
+  t.routes = t.options.routes;
+  var path = window.location.hash.substr(1);
+  path = path || t.options.defaultRoute;
+  t.navigate(path);
+  $(window).on('hashchange',function(e) {
+    var path = window.location.hash.substr(1);
+    path = path || t.options.defaultRoute;
+    t.navigate(path);
+  });
+}
+App.Router.prototype.navigate = function(path) {
+  console.log("Navigating to "+path);
+  var t = this;
+  var parts = path.split('/');
+  var page = parts.shift();
+  if(t.routes.hasOwnProperty(page)) {
+    t.routes[page].apply(t.context, parts);
+  }
+  this.options.onNavigate.apply(this);
+}
---
+++ app/script.js
@@ -0,0 +1,251 @@
+$(function() {
+
+  var $loading = $('#loading');
+  var $pageContainer = $('#pages');
+  var pageClass = 'page';
+  var activeClass = 'active';
+
+  window.app = new App();
+
+  var viewMixin = {
+    activeClass: 'active',
+    pageClass: 'page',
+    show: function() {
+      var t = this;
+      $pageContainer.find('.'+t.pageClass+'.'+t.activeClass).removeClass(t.activeClass);
+      t.$el.addClass(t.activeClass);
+      app.views.header.setTitle(t.title);
+      return t;
+    }
+  }
+  _.extend(App.View.prototype, viewMixin);
+
+  app.views = {};
+
+  app.views.menu = new App.View({
+    id: 'menu',
+    events: {
+      'click .menu-close': function(e) {
+        e.preventDefault();
+        console.log("menu click close");
+        this.$el.trigger('close');
+      },
+      'close': function() {
+        console.log("menu close");
+        $pageContainer.removeClass('menu-open');
+      },
+      'toggle': function(e) {
+        console.log("menu toggle");
+        if(e) { e.preventDefault(); }
+        $pageContainer.toggleClass('menu-open');
+      }
+    }
+  });
+
+  app.views.header = new App.View({
+    id: 'header',
+    events: {
+      'click .menu-toggle': function(e) {
+        e.preventDefault();
+        console.log("header toggle");
+        app.views.menu.$el.trigger('toggle');
+      },
+      'click .btn-back': function(e) {
+        e.preventDefault();
+        console.log("header back");
+        window.history.back();
+      },
+      'click .btn-search': function(e) {
+        e.preventDefault();
+        console.log("Open search");
+        var $s = this.$el.find('#search');
+        if($s.hasClass('open')) {
+          $s.find('input').blur();
+          $s.removeClass('open');
+          app.views.schedule.clearFilter();
+        } else {
+          $s.find('input').focus();
+          $s.addClass('open');
+        }
+      },
+      'click #search': function(e) {
+        var $i = $(e.target);
+        $i.find('input').blur().focus();
+      },
+      'keyup .search-input': function(e) {
+        var text = $(e.target).val();
+        if(text.length > 0) {
+          app.views.schedule.filter(text);
+        } else {
+          app.views.schedule.clearFilter();
+        }
+      }
+    },
+    // custom methods
+    setTitle: function(title) {
+      this.$el.find('h1').text(title);
+    },
+    hideSearch: function() {
+      this.$el.find('.btn-search').hide();
+      this.$el.find('#search').removeClass('open');
+    }
+  });
+
+  app.views.about = new App.View({
+    id: 'about',
+    title: 'About'
+  });
+
+  app.views.eventDetail = new App.View({
+    id: 'event-detail',
+    template: 'event-detail-template',
+    title: 'Event Detail',
+    // custom methods
+    render: function(model, id) {
+      var t = this;
+      t.$el.find('.page-content').html(
+        t.$template.render(model.data.lookup[id])
+      );
+    }
+  });
+
+  app.views.map = new App.View({
+    id: 'map',
+    title: 'Map',
+    init: function() {
+      this.map = L.mapbox.map('map-container', 'bgun.map-0xo3jced',{
+        attributionControl: false,
+        detectRetina: true,
+        retinaVersion: 'bgun.map-0xo3jced'
+      }).setView([34.02183,-84.32968],16);
+    }
+  });
+
+  app.views.schedule = new App.View({
+    id: 'schedule',
+    template: 'schedule-item-template',
+    title: 'Schedule',
+    // custom methods
+    filter: function(text) {
+      text = text.toLowerCase();
+      console.log(text);
+      var t = this;
+      t.$el.find('li.event .time').show();
+      t.$el.find('li.event').show().filter(function(i) {
+        var t = $(this).text().toLowerCase();
+        return t.indexOf(text) == -1;
+      }).hide();
+      t.$el.find('li.separator').hide();
+      if(t.$el.find('li:visible').length == 0) {
+        t.$el.find('.no-results').show();
+      } else {
+        t.$el.find('.no-results').hide();
+      }
+    },
+    clearFilter: function() {
+      var t = this;
+      t.$el.find('li').show();
+      t.$el.find('li.event .time').hide();
+      t.$el.find('.no-results').hide();
+    },
+    render: function(model) {
+      var t = this;
+      t.$el.find('#schedule-list').html(
+        t.$template.render(model.data.withSeparators)
+      );
+      return t;
+    }
+  });
+
+  app.models = {};
+  app.models.events = new App.Model({
+    url: 'http://con-nexus.com/api/13/events',
+    parse: function(r) {
+      var t = this;
+      // todo: models!
+      var items = r.items;
+      var a, arr = [];
+      var schedule = [];
+
+      // won't need this after API starts returning a sorted list
+      console.time("sort");
+      for(var i in items) {
+        arr.push(items[i]);
+      }
+      arr = _.sortBy(arr, function(i) {
+        return i.StartDate;
+      });
+      console.timeEnd("sort");
+
+      for(var i=0; i<arr.length; i++) {
+        a = arr[i];
+        a.fdate = moment(a.StartDate).format("dddd h:mm a");
+        if(i == 0) {
+          schedule.push({ type: "separator", fdate: moment(a.StartDate).format("dddd h:mm a")});
+        }
+        if(i > 0 && a.StartDate > arr[i-1].StartDate) {
+          schedule.push({ type: "separator", fdate: moment(a.StartDate).format("dddd h:mm a")});
+        }
+        schedule.push(a);
+      }
+      return {
+        lookup: items,
+        sorted: arr,
+        withSeparators: schedule
+      };
+    }
+  });
+
+  app.controllers = {};
+  app.controllers.schedule = function() {
+    var t = this;
+    t.views.header.$el.find('.btn-back').hide();
+    t.views.header.$el.find('.btn-search').show();
+    t.views.schedule.show();
+  }
+  app.controllers.about = function() {
+    var t = this;
+    t.views.header.$el.find('.btn-back').hide();
+    t.views.header.hideSearch();
+    t.views.schedule.clearFilter();
+    t.views.about.show();
+  }
+  app.controllers.eventDetail = function(id) {
+    var t = this;
+    t.views.eventDetail.render(t.models.events, id)
+    t.views.header.$el.find('.btn-back').show();
+    t.views.header.hideSearch();
+    t.views.schedule.clearFilter();
+    t.views.eventDetail.show();
+  }
+  app.controllers.map = function() {
+    var t = this;
+    t.views.header.$el.find('.btn-back').hide();
+    t.views.header.hideSearch();
+    t.views.map.show();
+  }
+
+  // init
+  app.models.events.load(function(data) {
+
+    $loading.hide();
+    app.views.schedule.render(app.models.events);
+
+    app.router = new App.Router({
+      context: app,
+      defaultRoute: "schedule",
+      routes: {
+        "about"        : app.controllers.about,
+        "schedule"     : app.controllers.schedule,
+        "event-detail" : app.controllers.eventDetail,
+        "map"          : app.controllers.map
+      },
+      onNavigate: function() {
+        app.views.menu.$el.trigger('close');
+      }
+    });
+    app.router.start();
+
+  });
+
+});
