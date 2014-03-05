facc448687907619a92c8920d64062f0239c39f9
Ben Gundersen <ben@bengundersen.com>
2014-03-05 01:53:18 -0500

    map locations

--- app/main.js
+++ app/main.js
@@ -138,11 +138,35 @@ function( $,        _,            mapbox,   jsrender,   moment,   FastClick,   A
     id: 'local-map',
     title: 'Local Map',
     init: function() {
-      this.map = L.mapbox.map('local-map-container', 'bgun.map-0xo3jced',{
+      var t = this;
+      var locations = [
+        {
+          lon: 34.0221599,
+          lat: -84.330078,
+          name: "DoubleTree by Hilton Atlanta-Roswell",
+          address: "1075 Holcomb Bridge Road<br />Roswell, Georgia, 30076",
+          phone: "770-992-9600"
+        }
+      ];
+      t.map = L.mapbox.map('local-map-container', 'bgun.map-0xo3jced',{
         attributionControl: false,
         detectRetina: true,
         retinaVersion: 'bgun.map-0xo3jced'
       }).setView([34.02183,-84.32968],16);
+
+      var marker;
+      var markerIcon = L.icon({
+        iconUrl: "./assets/images/map-marker.png",
+        iconSize: [33,48],
+        iconAnchor: [16,48],
+        popupAnchor: [0,-32]
+      });
+      _.each(locations, function(l) {
+        console.log(l);
+        marker = L.marker([l.lon, l.lat], {icon: markerIcon});
+        marker.bindPopup($('#map-popup-template').render(l));
+        t.map.addLayer(marker);
+      });
     }
   });
 
